2ebb5211f5e848a7ab59c1c2bdcc0a4f475a1682
celinenguyentu <celine.nguyentu@gmail.com>
2024-02-29 12:33:03 +0100

    C11

---
+++ C-11/mgama/ex04/do_op.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_op.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgama <mgama@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 19:28:38 by mgama             #+#    #+#             */
+/*   Updated: 2022/11/08 19:45:51 by mgama            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DO_OP_H
+# define DO_OP_H
+
+int		ft_add(int a, int b);
+int		ft_sub(int a, int b);
+int		ft_mod(int a, int b);
+int		ft_div(int a, int b);
+int		ft_mul(int a, int b);
+int		ft_atoi(char *str);
+void	ft_putnbr(int nb);
+
+#endif
