25d2c171f8e245661a8cbe829c51f1634cd58a1b
celinenguyentu <celine.nguyentu@gmail.com>
2024-03-10 17:55:17 +0100

    Add C06

---
+++ C06/README.md
@@ -0,0 +1,10 @@
+# C 06
+
+## Table of contents
+
+Exercise						| Score  
+--------------------------------|--------
+00 - ft_print_program_name		| OK
+01 - ft_print_params			| OK
+02 - ft_rev_params 				| OK
+03 - ft_sort_params				| OK
---
+++ C06/ex02/ft_rev_params.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnguyen- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 15:27:31 by cnguyen-          #+#    #+#             */
+/*   Updated: 2024/02/19 05:09:10 by cnguyen-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+		return (1);
+	i = argc - 1;
+	while (i > 0)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		i--;
+	}
+	return (0);
+}
